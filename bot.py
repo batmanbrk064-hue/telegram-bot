7ad9f08425efdf8e20d737db3b967665972fbafb
batmanbrk064-hue <batmanbrk064@gmail.com>
2026-02-24 00:59:43 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,10 @@
 import telebot
 from telebot import types
 
-TOKEN = "8780687512:AAEznDGiwZDKdelnTV9LbRhKLR2KITML-zg"
-CHANNEL_ID = -1003502571913
+TOKEN = "8780687512:AAEznDGiwZDKdelnTV9LbRhKLR2KITML-zg"  # حط التوكن تاعك
+CHANNEL_ID = -1003502571913    # ID القناة
 
 bot = telebot.TeleBot(TOKEN)
-
 user_state = {}
 
 @bot.message_handler(commands=['start'])
@@ -21,18 +20,24 @@ def start(message):
 def ask_details(message):
     user_state[message.chat.id] = message.text
     bot.send_message(message.chat.id,
-                     "✍️ اكتب تفاصيل الإعلان الآن:\n\nمثال:\nنوع الحساب:\nالمستوى:\nالسعر:")
+                     "✍️ اكتب تفاصيل الإعلان الآن:\nمثال:\nنوع الحساب:\nالسعر:")
 
 @bot.message_handler(func=lambda message: message.chat.id in user_state)
 def publish_ad(message):
     ad_type = user_state[message.chat.id]
     user = message.from_user
 
+    # تحديد الاسم المعروض
+    if user.username:
+        display_name = f"@{user.username}"
+    else:
+        display_name = f"{user.first_name} {user.last_name if user.last_name else ''}".strip()
+
     text = f"""
 🔥 إعلان جديد 🔥
 
 📌 النوع: {ad_type}
-👤 الناشر: @{user.username if user.username else "لا يوجد يوزر"}
+👤 الناشر: {display_name}
 🆔 ID: {user.id}
 
 📝 التفاصيل:
